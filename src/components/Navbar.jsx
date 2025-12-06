fd555fc640cec8f1f9b9034bc8c84de650be4be3
Hexa <ghstdu@gmail.com>
2025-12-06 11:06:37 +0100

    added preset custom colors

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,17 +11,17 @@ const Navbar = (props) => {
 
   return (
     <div className="bg-white min-h-screen min-w-[200px] shadow-[5px_5px_25px_5px_rgba(0,0,0,0.02)]">
-        <div className="border-1 border-[rgb(60,60,60)] w-[30px] h-[30px] mb-5 m-[5px]"></div>
+        <div className="border-1 border-accent w-[30px] h-[30px] mb-5 m-[5px]"></div>
         {props.pageList.map((e, index) => {
             const Icon = e.icon
             return (
             <div key={index} 
             onClick={()=>{clickHandler(index)}}
-            className={`flex flex-row items-center h-8 w-full pl-1.5 text-[15px] font-semibold cursor-pointer ${ Selected == index && "bg-[rgba(204,102,0,0.1)] [&>a]:text-[rgba(204,102,0,1)] [&>div]:border-[rgba(204,102,0,1)]"} hover:[&>div]:border-[rgba(204,102,0,1)] hover:[&>a]:text-[rgba(204,102,0,1)] hover:bg-[rgba(204,102,0,0.08)] backdrop-blur-xs transition-all duration-100`}>
-                <Icon className={`w-[15px] h-[15px] mr-1.5 text-[rgb(60,60,60)] ${ Selected == index && "text-[rgba(204,102,0,1)]"}`}/>
-                <a className="text-[.825em] font-[14px] text-[rgb(60,60,60)]">{e.name}</a>
+            className={`flex flex-row items-center h-8 w-full pl-1.5 text-[15px] font-semibold cursor-pointer ${ Selected == index && "bg-[rgba(204,102,0,0.1)] [&>a]:text-primary [&>div]:border-primary"} hover:[&>div]:border-primary hover:[&>a]:text-primary hover:bg-[rgba(204,102,0,0.08)] backdrop-blur-xs transition-all duration-100`}>
+                <Icon className={`w-[15px] h-[15px] mr-1.5 text-accent ${ Selected == index && "text-primary"}`}/>
+                <a className="text-[.825em] font-[14px] text-accent">{e.name}</a>
                 {Selected == index && (
-                    <span className="bg-[rgba(204,102,0,1)] w-[3px] h-full ml-auto"></span>
+                    <span className="bg-primary w-[3px] h-full ml-auto"></span>
                 )}
             </div>)
         })}
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -4,7 +4,7 @@ const Table = ({tableHead, rowData}) => {
   return (
     <>
       <div className="flex flex-col w-full rounded-[10px] bg-white [&>div]:rounded-[10px]">
-        <div className="w-full h-10 bg-[rgba(243,245,249,1)] rounded-t-[10px]">
+        <div className="w-full h-10 bg-secondary rounded-t-[10px]">
           <div></div>
         </div>
         <div className="w-full h-10">
