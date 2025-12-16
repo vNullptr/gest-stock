132cc739da0d9fbbeaadfc32df064909d3a98ac8
Hexa <ghstdu@gmail.com>
2025-12-16 18:49:29 +0100

    finished properly setting up and connecting to mysql server

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -38,7 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
-    "rest_framework"
+    "rest_framework",
+    
+    "accounts",
+    "order",
+    "product",
+    "shop",
+    
 ]
 
 MIDDLEWARE = [
@@ -75,9 +81,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "django_stock_db",
+        "USER": "stockuser",
+        "PASSWORD": "testpassword",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
 
