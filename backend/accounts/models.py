52a39824396f47e516b95bbc00119c02f110e726
Hexa <ghstdu@gmail.com>
2025-12-17 15:01:28 +0100

    made the staff model a abstract user

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,14 +1,12 @@
 from django.db import models
 from shop.models import Shop
+from django.contrib.auth.models import AbstractUser
 
-class Staff(models.Model):
+class Staff(AbstractUser):
     class Role(models.IntegerChoices):
         EMPLOYE = 0, 'Employe'
         RESPONSABLE = 1, 'Responsable'
         GERANT = 2, 'Gerant'
-        
-    password = models.CharField(max_length=100)
-    contact = models.CharField(max_length=10)
     role = models.SmallIntegerField(choices=Role.choices, default=Role.EMPLOYE)
     shop = models.ForeignKey(
         Shop,
