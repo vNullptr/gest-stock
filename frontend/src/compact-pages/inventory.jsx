8e6cf4754d283690a922b375862c45134a2e8537
unknown <ghstdu@gmail.com>
2025-12-18 09:51:10 +0100

    more advancement on floating windows

--- frontend/src/compact-pages/inventory.jsx
+++ frontend/src/compact-pages/inventory.jsx
@@ -3,10 +3,35 @@ import Table from '../components/Table.jsx'
 import Window from '../components/Window.jsx'
 
 const Inventory = () => {
+
+  const windowContent = () => (
+    <div className="w-full h-full p-5 flex flex-col w-full">
+      <h1 className="text-4xl font-bold ">Edit</h1>
+      <div className="w-full h-full flex py-5 items-between justify-center">
+        <form className="flex flex-col w-[80%]">
+          <label className="flex flex-col w-full">
+            test1
+            <input className="outline outline-gray-300 rounded-sm h-8 mb-5 "></input>
+          </label>
+          <div className="flex flex-row">
+            <label className="flex flex-col w-1/2 mr-5">
+              test2
+              <input className="outline outline-gray-300 rounded-sm h-8 w-full mb-5 "></input>
+            </label>
+            <label className="flex flex-col w-1/2">
+              test3
+              <input className="outline outline-gray-300 rounded-sm h-8 w-full mb-5 "></input>
+            </label>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+
   return (
     <div className="h-full p-5 flex flex-col">
         <Table/>
-        <Window/>
+        <Window WindowContent={windowContent}/>
     </div>
   )
 }
--- frontend/src/components/Window.jsx
+++ frontend/src/components/Window.jsx
@@ -9,8 +9,8 @@ const Window = ({WindowContent}) => {
     const Content = WindowContent ?? Placeholder
         
     return (
-        <div className="absolute top-0 left-0 w-screen h-screen bg-[rgba(0,0,0,0.3)] flex items-center justify-center">
-            <div className="w-[650px] h-5/6 bg-white rounded-lg">
+        <div className="absolute top-0 left-0 w-screen h-screen bg-[rgba(0,0,0,0.3)] flex items-center justify-center ">
+            <div className="w-[650px] h-5/6 bg-white rounded-lg overflow-hidden">
                 <Content/>
             </div>
         </div>
