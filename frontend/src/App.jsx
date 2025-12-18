2527e7274e9f413a3f45b240a36929976e8b6f6f
Hexa <ghstdu@gmail.com>
2025-12-18 19:32:05 +0100

    changed /main to just /

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import Login from './pages/Login'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Main from './pages/Main'
+import api from './api/axios'
+import {useNavigate} from 'react-router-dom'
 
 const App = () => {
+
   return (
     <Router>
 
       <Routes>
         <Route path="/login" element={<Login/>}></Route>
-        <Route path="/main" element={<Main/>}></Route>
+        <Route path="/" element={<Main/>}></Route>
       </Routes>
 
     </Router>
