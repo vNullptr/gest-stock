1a4a4e5200574af80b17d0718c0b6e4b618ca5a1
Hexa <ghstdu@gmail.com>
2025-12-18 19:33:14 +0100

    styled table again ...

--- frontend/src/components/Table.jsx
+++ frontend/src/components/Table.jsx
@@ -1,38 +1,36 @@
 import React from 'react'
 
-const Table = ({tableHead, rowData}) => {
-
-  const listElements = []
-  for (let i = 0; i < 25; i++) {
-    listElements.push(
-    <tr className="[&>td]:p-2 [&>td]:text-left [&>td]:font-light [&>td]:text-sm shadow-sm h-15 rounded-lg">
-      <td>{i}</td>
-      <td>Data</td>
-      <td>Data</td>
-      <td>Data</td>
-      <td>
-        <div className="flex flex-row justify-end items-center">
-          <button className="w-5 h-5 border border-black"></button>
-        </div>
-      </td>
-    </tr>)
-  }
+const Table = ({Headers, Data, editHandler}) => {
 
   return (
     <>
-      <div className='overflow-scroll'>
-        <table className='bg-white w-full rounded-lg select-none border-separate border-spacing-5'>
-          <thead className="bg-white">
-            <tr className=" [&>td]:p-2 [&>td]:font-bold [&>td]:text-left [&>td]:text-sm">
-              <td className="w-[5%]">ID</td>
-              <td className="w-[50%]">Nom Produit</td>
-              <td className="W-[15%]">Stock</td>
-              <td className="W-[15%]">Prix</td>
-              <td className="W-[15%]"></td>
+      <div className="overflow-scroll bg-white rounded-xl shadow-sm p-6">
+        <table className="w-full rounded-lg select-none">
+          <thead className="">
+            <tr className="[&>td]:px-5 text-sm font-bold text-left text-gray-600 h-10">
+              {Headers.map((v)=>{
+                return <td key={v.key} className={`w-[${v.width}%]`}>{v.label}</td>
+              })}
+              <td></td>
             </tr>
           </thead>
-          <tbody className="">
-            {listElements}
+          <tbody>
+            {Data !== undefined &&
+              Data.map((row,i)=>{
+                return (
+                  <tr key={i} className="[&>td]:px-5 text-sm text-left text-gray-500 h-15">
+                  {Headers.map((header)=>{
+                    return <td>{row[header.key]}</td>
+                  })}
+                  <td>
+                    <div className="flex flex-row justify-end items-center">
+                      <button className="w-5 h-5 border border-black" onClick={()=>{editHandler(row["id"])}}></button>
+                    </div>
+                  </td>
+                </tr>
+                )
+              })
+            }
           </tbody>
         </table>
       </div>
