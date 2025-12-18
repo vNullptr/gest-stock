62b6f20ff1125eba0039cfe0fd06321db7e0c554
Hexa <ghstdu@gmail.com>
2025-12-18 19:34:47 +0100

    refactored window component to use child instead of a prop

--- frontend/src/components/Window.jsx
+++ frontend/src/components/Window.jsx
@@ -1,17 +1,15 @@
 import React from 'react'
 
-const Window = ({WindowContent}) => {
+const Window = ({children}) => {
 
     const Placeholder = () => (
       <div className="p-6 text-center text-accent opacity-50">Page not implemented yet</div>
     )
-
-    const Content = WindowContent ?? Placeholder
         
     return (
         <div className="absolute top-0 left-0 w-screen h-screen bg-[rgba(0,0,0,0.3)] flex items-center justify-center ">
             <div className="w-[650px] bg-white rounded-lg overflow-hidden">
-                <Content/>
+                {children ?? Placeholder}
             </div>
         </div>
     )
