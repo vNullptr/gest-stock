45017f27773ff8fbaaa9e7dc3380216e8b6aae19
Hexa <ghstdu@gmail.com>
2025-12-18 19:35:21 +0100

    linked the inventory component with the api and some refactoring too

--- frontend/src/compact-pages/inventory.jsx
+++ frontend/src/compact-pages/inventory.jsx
@@ -1,52 +1,59 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Table from '../components/Table.jsx'
 import Window from '../components/Window.jsx'
+import api from '../api/axios.js'
+import EditForm from '../components/forms/editForm.jsx'
 
 const Inventory = () => {
 
-  const windowContent = () => (
-    <div className="w-full h-full p-5 flex flex-col w-full">
-      <div className="w-full h-full flex flex-col py-5 items-center">
-        
-        <form className="flex flex-col w-[80%]">
-          <h1 className="text-3xl mb-7 text-primary">Modifier</h1>
-          <div className="w-full flex flex-col">
-            <label className="font-extralight text-sm">Nom produit</label>
-            <input className="outline outline-gray-300 rounded-sm h-8 mb-5 "></input>
-          </div>
+  const [selectedProduct, setselectedProduct] = useState(null)  
+  const [Products, setProducts] = useState([])
+  const productHeader = [
+    {label:"ID", key:"id", width:10}, 
+    {label:"Nom produit", key:"name" , width:40}, 
+    {label:"Categorie", key:"category" , width:20}, 
+    {label:"Prix", key:"price" , width:15}, 
+    {label:"Quantite", key:"quantity" , width:15}
+  ]
 
-          <div className="w-full flex flex-col">
-            <label className="font-extralight text-sm">Categorie</label>
-            <select className="outline outline-gray-300 rounded-sm h-8 mb-5">
-              <option>1</option>
-              <option>2</option>
-              <option>3</option>
-            </select>
-          </div>
+  const editHandler = (id)=>{
+    setselectedProduct(id)
+  }
 
-          <div className="flex flex-row justify-between">
-            <div>
-              <label className="font-extralight text-sm">Prix</label>
-              <input className="outline outline-gray-300 rounded-sm h-8 w-full mb-5 "></input>
-            </div>
-            <div>
-              <label className="font-extralight text-sm">Quantite</label>
-              <input className="outline outline-gray-300 rounded-sm h-8 w-full mb-5 "></input>
-            </div>
-          </div>
-          <div className="w-full h-13 flex flex-row items-center justify-end space-x-2 mt-5">
-            <button className="rounded-md ring text-primary  h-8 w-20 ring-primary">Annuler</button>
-            <button className="rounded-md bg-primary text-white  h-8 w-20">Appliquer</button>
-          </div>
-        </form>
-      </div>
-    </div>
-  )
+  useEffect(()=>{
+    const fetchProducts = async ()=>{
+    const token = sessionStorage.getItem("session-token")
+    if (token) {
+      try {
+        const response = await api(token).get('/api/product')
+        response.data.map((v, _)=>{
+          Object.values(v).map((v,_)=>{
+            if (v === null){
+              return ""
+            }
+          })
+        })
+        setProducts(response.data)
+      } catch (err){
+        console.error(err)
+      }
+    } else {
+      navigate('/login')
+    }
+      
+    }
+
+    fetchProducts()
+  },[])
+
 
   return (
     <div className="h-full p-5 flex flex-col">
-        <Table/>
-        <Window WindowContent={windowContent}/>
+        <Table Headers={productHeader} Data={Products} editHandler={editHandler}/>
+        {  selectedProduct != null && 
+        <Window>
+          <EditForm currentProductId={selectedProduct} onClose={()=>{setselectedProduct(null)}} />
+        </Window>}
     </div>
   )
 }
