1ce2d14e6d1482595c92bad3bb84614f8f222dc4
Hexa <ghstdu@gmail.com>
2025-12-18 19:36:13 +0100

    added new endpoint to fetch a singular product by id

--- backend/product/urls.py
+++ backend/product/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     path('product/', views.getProduct)
+     path('product/', views.getProducts),
+     path('product/<int:product_id>/', views.getProduct)
 ]
--- backend/product/views.py
+++ backend/product/views.py
@@ -2,10 +2,17 @@ from rest_framework.response import Response
 from product.models import Product
 from .serializers import ProductSerializer
 from rest_framework.decorators import api_view
+from django.shortcuts import get_object_or_404
 
 
 @api_view(['GET'])
-def getProduct(request):
+def getProducts(request):
     products = Product.objects.all()
     serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getProduct(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    serializer = ProductSerializer(product)
     return Response(serializer.data)
