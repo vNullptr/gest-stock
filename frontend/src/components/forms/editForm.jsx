0f69183ea3467ded994d47837c0af8510439f63c
vNullptr <ghstdu@gmail.com>
2025-12-19 21:49:40 +0100

    fixed the input fields + implemented the patch + category fetching

--- frontend/src/components/forms/editForm.jsx
+++ frontend/src/components/forms/editForm.jsx
@@ -3,12 +3,8 @@ import api from '../../api/axios'
 
 const EditForm = ({currentProductId, onClose}) => {
 
-  const [Product, setProduct] = useState({
-    name:"",
-    price:"",
-    quantity:"",
-    category:""
-  })
+  const [Product, setProduct] = useState({})
+  const [Categories, setCategories] = useState([])
 
   useEffect(()=>{
 
@@ -16,8 +12,11 @@ const EditForm = ({currentProductId, onClose}) => {
       const token = sessionStorage.getItem("session-token")
       if (token) {
         try {
-          const response = await api(token).get(`/api/product/${currentProductId}`)
-          setProduct(response.data)
+          const prodResponse = await api(token).get(`/api/products/${currentProductId}`)
+          setProduct(prodResponse.data)
+          const catResponse = await api(token).get(`/api/categories`)
+          setCategories(catResponse.data)
+
         }catch (err){
           console.error(err)
           onClose()
@@ -34,6 +33,24 @@ const EditForm = ({currentProductId, onClose}) => {
 
   const clickHandler = ()=>{
 
+    const patchProduct = async () =>{
+      const token = sessionStorage.getItem("session-token")
+      if (token) {
+        try {
+          await api(token).patch(`/api/products/${currentProductId}/`,Product)
+          console.log(Product)
+          onClose()
+        }catch (err){
+          console.error(err)
+          onClose()
+        }
+      } else {
+        console.error("no session token")
+        onClose()
+      }
+    }
+
+    patchProduct()
   }
 
   return (
@@ -46,31 +63,32 @@ const EditForm = ({currentProductId, onClose}) => {
           <h1 className="text-3xl mb-7 text-primary">Modifier</h1>
           <div className="w-full flex flex-col">
             <label className="font-extralight text-sm">Nom produit</label>
-            <input className="outline outline-gray-300 rounded-sm h-8 mb-5 p-2" value={Product["name"]} onChange={(e)=>{}} ></input>
+            <input className="outline outline-gray-300 rounded-sm h-8 mb-5 p-2" value={Product?.name} onChange={(e)=>{setProduct(prev => ({...prev, name:e.target.value}))}} ></input>
           </div>
 
           <div className="w-full flex flex-col">
             <label className="font-extralight text-sm">Categorie</label>
-            <select className="outline outline-gray-300 rounded-sm h-8 mb-5 p-2">
-              <option>{Product["category"]}</option>
-              <option>2</option>
-              <option>3</option>
+            <select className="outline outline-gray-300 rounded-sm h-8 mb-5 p-2" onChange={(e)=>{setProduct(prev=>({...prev, category:Categories.find(cat => cat?.name === e.target.value)?.id }))}}>
+              <option>{Categories.length > 0 && Product?.category && (Categories.find((v, i)=> v?.id == Product?.category)?.name)}</option>
+              {Categories.map((cat, k)=>{
+                return cat?.id != Product?.category && (<option>{cat?.name}</option>)
+              })}
             </select>
           </div>
 
           <div className="flex flex-row justify-between space-x-3">
             <div>
               <label className="font-extralight text-sm">Prix</label>
-              <input className="outline outline-gray-300 rounded-sm h-8 w-full mb-5 p-2" value={Product["price"]} onChange={(e)=>{}}></input>
+              <input className="outline outline-gray-300 rounded-sm h-8 w-full mb-5 p-2" type="number" value={Product?.price} onChange={(e)=>{setProduct(prev => ({...prev, price:e.target.value}))}}></input>
             </div>
             <div>
               <label className="font-extralight text-sm">Quantite</label>
-              <input className="outline outline-gray-300 rounded-sm h-8 w-full mb-5 p-2" value={Product["quantity"]} onChange={(e)=>{}}></input>
+              <input className="outline outline-gray-300 rounded-sm h-8 w-full mb-5 p-2" type="number" value={Product?.quantity} onChange={(e)=>{setProduct(prev => ({...prev, quantity:e.target.value}))}}></input>
             </div>
           </div>
           <div className="w-full h-13 flex flex-row items-center justify-end space-x-2 mt-5">
-            <button className="rounded-md ring text-primary  h-8 w-20 ring-primary hover:scale-105 transition-all duration-200" onClick={onClose}>Annuler</button>
-            <button className="rounded-md bg-primary text-white  h-8 w-20 hover:bg-[#ff9900] hover:scale-105 transition-all duration-200" onClick={clickHandler}>Appliquer</button>
+            <button className="rounded-md ring text-primary  h-8 w-20 ring-primary hover:scale-105 transition-all duration-200" type="button" onClick={onClose}>Annuler</button>
+            <button className="rounded-md bg-primary text-white  h-8 w-20 hover:bg-[#ff9900] hover:scale-105 transition-all duration-200" type="button" onClick={clickHandler}>Appliquer</button>
           </div>
         </form>
       </div>
