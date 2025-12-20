5ca8c3119555035c1984a9cab9cd9bf67db716a6
vNullptr <ghstdu@gmail.com>
2025-12-20 16:32:06 +0100

    refactored serializers to use ModelSerializer and added ShopViewSet for API integration

--- backend/shop/serializers.py
+++ backend/shop/serializers.py
@@ -1,12 +1,12 @@
 from rest_framework import serializers
 from shop.models import Shop, Stock
 
-class ShopSerializer(serializers.Serializer):
+class ShopSerializer(serializers.ModelSerializer):
     class Meta:
         model = Shop
         fields = '__all__'
 
-class StockSerializer(serializers.Serializer):
+class StockSerializer(serializers.ModelSerializer):
     class Meta:
         model = Stock
         fields = '__all__'
--- backend/shop/views.py
+++ backend/shop/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from .models import Shop
+from .serializers import ShopSerializer
+from rest_framework import viewsets
 
-# Create your views here.
+class ShopViewSet(viewsets.ModelViewSet):
+    queryset = Shop.objects.all()
+    serializer_class = ShopSerializer
+
