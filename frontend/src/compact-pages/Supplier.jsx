3e2a5d40fafe035d4ee1a8fece12197c4b499273
vNullptr <ghstdu@gmail.com>
2025-12-21 18:32:16 +0100

    feat: add Supplier page and integrate it into the Main component

---
+++ frontend/src/compact-pages/Supplier.jsx
@@ -0,0 +1,45 @@
+import React, {useState, useEffect} from 'react'
+import OrderForm from '../components/forms/orderForm'
+import OrderCard from '../components/OrderCard'
+import api from '../api/axios'
+
+const Supplier = () => {
+
+    const [Orders, SetOrders] = useState([])
+
+    useEffect(()=>{
+        const fetchOrders = async ()=>{
+            const token = sessionStorage.getItem("session-token")
+            if (token) {
+                try {
+                    const response = await api(token).get(`/api/orders/`)
+                    console.log(response.data[0])
+                    SetOrders(response.data)
+                } catch (err){
+                    console.error(err)
+                }
+            } else {
+                navigate('/login')
+            }
+        }
+
+        fetchOrders()
+    }, [])
+
+  return (
+    <div className="p-2">
+        <div className="flex flex-col space-y-2 h-full ">
+            <div className="p-4 bg-white rounded-sm shadow-sm w-full">
+                <OrderForm/>
+            </div>
+            <div className="p-3 bg-white rounded-sm shadow-sm w-full flex flex-col space-y-3 h-[700px] overflow-y-scroll">
+                {Orders.map(Order=>{
+                    return <OrderCard Data={Order}/>
+                })}
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Supplier
