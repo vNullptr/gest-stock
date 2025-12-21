fb86b3875e2ee15c789d06e9ce0531a2ef7c9cac
vNullptr <ghstdu@gmail.com>
2025-12-21 18:32:41 +0100

    feat: implement order creation with random arrival date and update serializers

--- backend/order/serializers.py
+++ backend/order/serializers.py
@@ -1,12 +1,32 @@
 from rest_framework import serializers
-from order.models import Order, Dealer
+from .models import Order, Dealer
+from product.serializers import ProductSerializer
+from shop.serializers import ShopSerializer
+import random
+from datetime import date, timedelta
 
-class OrderSerializer(serializers.ModelSerializer):
+def random_arrival_date():
+    return date.today() + timedelta(days=random.randint(3, 20))
+
+class OrderCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
-        fields = '__all__'
+        fields = ["product", "shop", "dealer", "quantity"]
         
+    def create(self, validated_data):
+        validated_data['date_arrival'] = random_arrival_date()
+        return super().create(validated_data)
+                
 class DealerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Dealer
         fields = '__all__'
+
+class OrderReadSerializer(serializers.ModelSerializer):
+    product = ProductSerializer(read_only=True)
+    shop = ShopSerializer(read_only=True)
+    dealer = DealerSerializer(read_only=True)
+    
+    class Meta:
+        model = Order
+        fields = ["id", "dealer", "product", "shop","quantity", "status", "date_arrival", "date_order"]
--- backend/order/views.py
+++ backend/order/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from .models import Dealer, Order
+from .serializers import DealerSerializer, OrderCreateSerializer, OrderReadSerializer 
+from rest_framework import viewsets
+from rest_framework.decorators import action
 
-# Create your views here.
+
+class SupplierViewSet(viewsets.ModelViewSet):
+    queryset = Dealer.objects.all()
+    serializer_class = DealerSerializer
+    
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    
+    def get_serializer_class(self):
+        if self.action == "create":
+            return OrderCreateSerializer
+        return OrderReadSerializer
+        
+        
+        
+        
+    
+    
