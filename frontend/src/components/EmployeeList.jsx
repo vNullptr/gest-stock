868cda4d9cf708d8d7fc58bba6c879353b82a826
vNullptr <ghstdu@gmail.com>
2025-12-21 14:58:52 +0100

    added StockTable and EmployeeList components to display shop stock and staff information

---
+++ frontend/src/components/EmployeeList.jsx
@@ -0,0 +1,54 @@
+import React, {useState, useEffect} from 'react'
+import api from '../api/axios'
+import Staff from '../compact-pages/Staff'
+
+const EmployeeList = ({SelectedShop}) => {
+
+    const [StaffList, setStaffList] = useState([])
+
+    useEffect(()=>{
+    const fetchStaff = async ()=>{
+      const token = sessionStorage.getItem("session-token")
+      if (token) {
+        try {
+          const response = await api(token).get(`/api/shop/${SelectedShop}/staff`)
+          console.log(response.data)
+          setStaffList(response.data)
+        } catch (err){
+          console.error(err)
+        }
+      } else {
+        navigate('/login')
+      }
+   
+    }
+    fetchStaff()
+  }, [SelectedShop])
+
+  return (
+    <>
+        <div className="flex flex-col space-y-2 w-full p-1">
+            <div className="h-8 w-full rounded-sm flex flex-row items-center justify-between font-bold text-gray-600">
+                    <div className="max-w-8 w-8 text-center ml-2">ID</div>
+                    <div className="max-w-8 w-8 text-left">Prenom</div>
+                    <div className="max-w-8 w-8 text-left">Nom</div>
+                    <div className="text-left w-[50%]"></div>
+                </div>
+            {StaffList.map(Staff => {
+                return <div className="h-8 w-full outline outline-gray-200 rounded-sm flex flex-row items-center justify-between">
+                    <div className="max-w-8 w-8 text-center ml-2">{Staff?.id}</div>
+                    <div className="max-w-8 w-8 text-left">{Staff?.first_name}</div>
+                    <div className="max-w-8 w-8 text-left">{Staff?.last_name}</div>
+                    <div className="text-left w-[50%] flex justify-end p-3">
+                        {
+                            Staff?.role >= 1 ? <span className="w-3 h-3 rounded-full bg-primary"></span> : ""
+                        }
+                    </div>
+                </div>
+            })}
+        </div>
+    </>
+  )
+}
+
+export default EmployeeList
