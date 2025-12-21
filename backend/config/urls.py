933440d06daeaed6f84214ea8ce89ab3be4b31c9
vNullptr <ghstdu@gmail.com>
2025-12-21 18:32:31 +0100

    feat: add order URLs to handle suppliers and orders

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('api/', include('product.urls')),
     path('api/', include('accounts.urls')),
     path('api/', include('shop.urls')),
+    path('api/', include('order.urls')),
 ]
